617fb6659477c27bba45e7bb639ea5c3eb854eb4
arkaitz_allua <arkaitz.allua.mayoral@gmail.com>
2026-06-14 12:02:16 +0200

    feat: init data models added
    MetricSnapshot, ProcessSnapshot, Alert, AlertEvent models
    AlertMetricType, AlertCondition, ProcessStatus enums
    One-to-many relationship between Alert and AlertEvent

---
+++ SystemMonitor/SystemMonitor.Data/Models/AlertCondition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemMonitor.Data.Models;
+
+public enum AlertCondition
+{
+    GreaterThan,
+    LessThan
+}
---
+++ SystemMonitor/SystemMonitor.Data/Models/MetricSnapshot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemMonitor.Data.Models;
+
+public class MetricSnapshot
+{
+    #region Attributes
+
+    public int Id { get; set; }
+    public DateTime CapturedAt { get; set; } = DateTime.UtcNow;
+
+    public double CpuUsagePercent { get; set; }
+    public long MemoryUsedBytes { get; set; }
+    public long MemoryTotalBytes { get; set; }
+
+    public long NetworkBytesReceived { get; set; }
+    public long NetworkBytesSent { get; set; }
+
+    public long DiskBytesRead { get; set; }
+    public long DiskBytesWritten { get; set; }
+
+    #endregion
+
+    #region Computed Properties
+
+    public double MemoryUsagePercent => MemoryTotalBytes > 0 ? (double)MemoryUsedBytes / MemoryTotalBytes * 100.0 : 0;
+
+    #endregion
+}
