8fd45cbbf6a424bbc5efce8de02a9d9abdd9a147
arkaitz_allua <arkaitz.allua.mayoral@gmail.com>
2026-06-15 13:40:58 +0200

    feat: bd initial migration

---
+++ SystemMonitor/SystemMonitor.Data/AppDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace SystemMonitor.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>().UseSqlite("DataSource=systemmonitor.db").Options;
+            return new AppDbContext(options);
+        }
+    }
+}
